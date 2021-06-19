3db0ba70ec864fa321b69513a47337d2fd48bdea
Sean O'Donnell <contact@seanodonnell.co.uk>
2021-06-19 11:24:32 +0100

    Created the database migration for the feed urls

---
+++ database/migrations/2021_06_19_102155_create_feed_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFeedTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feed', function (Blueprint $table) {
+            $table->id();
+            $table->string('url'); // Our feed url
+            $table->boolean('active')->default(true); // whether the feed is active or not
+            $table->timestamp('last_checked'); // Update this timestamp whenever we pull the latest articles
+            $table->timestamps();
+
+            $table->index(['active']); // Indexing active as we'll only be getting the active feeds
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feed');
+    }
+}
