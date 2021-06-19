5e4be88219be0eb07a74d01aceec97c7983845e0
Sean O'Donnell <contact@seanodonnell.co.uk>
2021-06-19 11:56:45 +0100

    Remove attempt at different validation

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -18,17 +18,16 @@ class APIController extends Controller
         return ['success' => true, 'feeds' => $feeds];
     }
 
+    /**
+     * @param Request $request
+     * @return array|\Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     * Post a feed URL into the API
+     */
     public function postFeed(Request $request) {
         // Validate input
         $request->validate([
             'url' => 'required|max:255'
         ]);
-
-        if ($request->fails()) {
-            return redirect('post/create')
-                ->withErrors($request)
-                ->withInput();
-        }
 
         $feed = Feed::create([
             'url' => $request->input('url')
