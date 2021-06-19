f435f409f8a16b3fd94c8c1ae1aa6b31db19d69c
Sean O'Donnell <contact@seanodonnell.co.uk>
2021-06-19 13:53:44 +0100

    Pressing ALT key will now show buttons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -47,7 +47,7 @@
                     </h3>
                     <div class="mb-1 text-muted">{{\Carbon\Carbon::parse($a->published_date)->diffForHumans()}}</div>
                     <p class="card-text mb-auto">{{Str::limit($a->description, 120)}}</p>
-                    <a href="{{$a->link}}">Continue reading</a>
+                    <a class="btn btn-primary btn_reading" style ="display:none" href="{{$a->link}}">Continue reading</a>
                     <small class="text-muted">Accreditation: {{$a->feed->url}}</small>
                 </div>
                 <!-- Having to use a random image source because the BBC doesn't seem to give out their images -->
@@ -72,6 +72,29 @@
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
 
+<!--
 
+Javascript to listen for ALT key press, native JS only, so can't use Jquery
+Can't use KeyPress as keypress doesn't work for command keys, (alt, shift etc..)
+Depressed = Pressed down
+
+ -->
+<script type="text/javascript">
+
+    document.addEventListener("keydown", function(e) {
+        if (e.altKey) {
+            console.log("Alt Key Pressed");
+            var buttons = document.getElementsByClassName("btn_reading");
+
+            Array.prototype.forEach.call(buttons, function(el) {
+                el.style.display = 'block';
+            });
+
+
+        }
+
+    });
+
+</script>
 
 <svg xmlns="http://www.w3.org/2000/svg" width="200" height="250" viewBox="0 0 200 250" preserveAspectRatio="none" style="display: none; visibility: hidden; position: absolute; top: -100%; left: -100%;"><defs><style type="text/css"></style></defs><text x="0" y="13" style="font-weight:bold;font-size:13pt;font-family:Arial, Helvetica, Open Sans, sans-serif">Thumbnail</text></svg></body></html>
